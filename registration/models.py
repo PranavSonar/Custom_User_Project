844f19bb693ebde9c72954080a97b83629c892b1
mia-k-monaghan <emmykperdue@gmail.com>
2020-11-24 14:45:10 -0700

    Migrations Level 2

--- registration/models.py
+++ registration/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    email = models.EmailField(_('email address'), unique=True)
+
+    def __str__(self):
+        return (self.email)
+
+    def save(self, *args, **kwargs):
+        """Populates username field with email. """
+        if not self.username:
+            self.username = self.email
+        super(CustomUser,self).save(*args,**kwargs)
