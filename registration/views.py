98a2c697e524d2ad09e2abb611762d1de9b4c9ad
mia-k-monaghan <emmykperdue@gmail.com>
2020-11-24 15:46:59 -0700

    Added Views

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import login, authenticate
+from django.views.generic import CreateView
+
+from .forms import SignUpForm, LoginForm
 # Create your views here.
+
+class SignupView(CreateView):
+    form_class = SignUpForm
+    template_name = 'users/signup.html'
+
+    def get_success_url(self):
+        return reverse_lazy('core:shipping')
+
+    def form_valid(self, form):
+        valid = super(SignupView, self).form_valid(form)
+        email, password = form.cleaned_data.get('email'), form.cleaned_data.get('password1')
+        user = authenticate(email=email,
+                            password=password)
+        login(self.request, user)
+        return valid
+
+
+class LoginView(auth_views.LoginView):
+    form_class = LoginForm
+    template_name = 'registration/login.html'
