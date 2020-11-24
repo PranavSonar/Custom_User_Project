90a4ae843b7fe385990365e9144b0620804c3c12
mia-k-monaghan <emmykperdue@gmail.com>
2020-11-24 16:36:12 -0700

    updated urls

--- registration/urls.py
+++ registration/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import LoginView, SignupView
+from django.contrib.auth import views as auth_views
 
 app_name = 'registration'
 
 urlpatterns = [
     path('', SignupView.as_view(), name='index'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
