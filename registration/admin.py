69ea39a8e4640dff6673c772aef7ae55ee547470
mia-k-monaghan <emmykperdue@gmail.com>
2020-11-28 14:22:32 -0700

    updated admin

--- registration/admin.py
+++ registration/admin.py
@@ -23,7 +23,3 @@ class UserAdmin(DjangoUserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
-
-
-# Register your models here.
-# admin.site.register(User, UserAdmin)
